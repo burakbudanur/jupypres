549df684b19c0f339ef095dae697183f08394f3c
Burak Budanur <burakbudanur@gmail.com>
2021-03-04 22:20:13 +0100

    added jupypres and a notebook illustrating its usage

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # jupypres
 A presentation tool (mostly matplotlib wrappers) for use with nbconvert --to slides
+
+`examples/jupypres.ipynb` demonstrates its usage.
---
+++ jupypres.py
@@ -0,0 +1,88 @@
+from matplotlib import pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.patches import FancyArrowPatch 
+import matplotlib as mpl
+import numpy as np 
+from IPython.display import Video
+
+current_figure = None
+
+def slide(layout, size=(16, 9), title=None):
+
+    global current_figure
+
+    sl = plt.figure(figsize=size, FigureClass=Slide).subplot_mosaic(layout)
+    fig = plt.gcf()
+
+    empty   = ['E', 'F', 'G', 'H']
+    text    = ['T', 'U', 'V', 'W']
+
+    for label in sl:
+        if (label in empty + text):
+            sl[label].axis("off")
+        else: 
+            sl[label].tick_params(labelsize=16)
+    
+    if title:
+        fig.suptitle("\n"+title+"\n", fontsize=36, linespacing=0.25)
+
+    current_figure = fig
+
+    return sl
+
+
+def duplicate_slide():
+
+    global current_figure
+
+    fig = current_figure 
+    slide = {}
+    
+
+    for ax in fig.axes:
+        slide[ax.get_label()] = ax
+
+    return slide, fig
+
+
+def show():
+
+    plt.show()
+
+
+def cross(ax, x, y, length=1.0, lw=1.0, color='red', yscale=1.0):
+
+    ax.plot(np.array([x - length, x + length]), 
+            np.array([y - length, y + length]) * yscale, 
+            color=color, lw=lw)
+
+    ax.plot(np.array([x + length, x - length]), 
+            np.array([y - length, y + length]) * yscale,
+            color=color, lw=lw)
+
+
+def arrow(ax, x, y, 
+angle=0, scale=1.0, mutation_scale=10, color='red', lw=4.0, arrowstyle='-|>'):
+
+    arr = FancyArrowPatch((x, y), 
+                          (x + scale * np.cos(angle), 
+                           y + scale * np.sin(angle)),
+                          mutation_scale=mutation_scale,
+                          facecolor=color,
+                          edgecolor=color,
+                          arrowstyle=arrowstyle,
+                          lw=lw
+                          )
+    ax.add_patch(arr)
+
+
+Video = Video
+
+class Slide(Figure):
+    """
+    See:
+    ---------
+    matplotlib.pyplot.figure
+    """
+
+    
